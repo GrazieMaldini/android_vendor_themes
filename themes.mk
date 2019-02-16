9f2615df5bd5657fffa4551d3aaa48060c593abb
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2019-02-16 14:16:42 -0500

    vendor: add Wellbeing to BlackAF
    Change-Id: Icaf97a2584dd85e666482dcd8a34a110393599f7
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- themes.mk
+++ themes.mk
@@ -43,7 +43,8 @@ PRODUCT_PACKAGES += \
     GBoardBlackAF \
     SettingsBlackAF \
     SystemBlackAF \
-    SettingsIntelligenceBlackAF
+    SettingsIntelligenceBlackAF \
+    WellbeingBlackAF
 
 # Switch themes
 PRODUCT_PACKAGES += \
